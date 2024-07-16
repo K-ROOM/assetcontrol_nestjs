a61cb9f63d153cff89cf455215cbaf026c7f7046
k-room <ronakorn.steam@hotmail.com>
2024-07-16 10:31:32 +0700

    asdfgbvcxzqwert

--- .history/src/login/user/user.service_20240716103122.ts
+++ .history/src/login/user/user.service_20240716103122.ts
@@ -87,7 +87,7 @@ export class UserService {
 
     try {
       // ระบุที่อยู่ของไฟล์รูปภาพใน Docker container
-      const filePath = join(__dirname, '..', 'EmployeePhotos', `${filename}.jpg`);
+      const filePath = join(__dirname, '.', 'EmployeePhotos', `${filename}.jpg`);
 
       // ดึงข้อมูลรูปภาพจาก path
       const fileData = await fsPromises.readFile(filePath);
